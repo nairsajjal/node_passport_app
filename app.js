5c9a0d1c2092ee81660fb730ed08fb1944f6f77e
sajjal <nairsajjal@gmail.com>
2019-06-25 23:59:08 +0530

    small commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-
+const mongoose = require('mongoose');
 const app = express();
 //ejs
 app.use(expressLayouts);
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+
+    MongoURI="mongodb+srv://sajjal:WNzr4c%401@database-xzf5e.mongodb.net/test?retryWrites=true&w=majority"
+};
