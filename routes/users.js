0e1cba19191e49580006fcdb46826c6db4cffdeb
sajjal <nairsajjal@gmail.com>
2019-06-25 17:11:35 +0530

    all views done

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express =  require("express");
+
+const router = express.Router();
+//login Page
+router.get('/login', (req,res) => {
+    res.render('login')
+});
+//Register Page
+router.get('/register', (req,res) => {
+    res.render('register')
+});
+module.exports = router;
