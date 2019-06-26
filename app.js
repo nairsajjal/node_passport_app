924d6150628ba21b5517d67a6fc203ed677f91b7
sajjal <nairsajjal@gmail.com>
2019-06-26 11:56:00 +0530

    final commit moved onto new project

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ mongoose.connect(db, {useNewUrlParser: true})
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+//BodyParser
+app.use(express.urlencoded({extended: false}));
+
+
 //routes 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express =  require("express");
 
 const router = express.Router();
+
+const bcrypt  = require('bcryptjs');
+//User model 
+const User = require('../models/User');
 //login Page
 router.get('/login', (req,res) => {
     res.render('login')
@@ -8,5 +12,63 @@ router.get('/login', (req,res) => {
 //Register Page
 router.get('/register', (req,res) => {
     res.render('register')
+});
+
+//Register Handle
+router.post('/register', (req,res)=> {
+    const{name, email, password, password2 } = req.body;
+    let errors= [];
+
+    //check required fields
+    if(!name || !email || !password || !password2){
+        errors.push({msg: 'Please fill in all fields'});
+
+    }
+    if(password!=password2){
+        errors.push({msg: 'Passwords dont match nigger'});
+
+    }
+    if(password.length < 6){
+        errors.push({msg: 'Password should be atleast 6 charachters'});
+
+    }
+    if(errors.length > 0)
+    {
+        res.render('register',{
+            errors,
+            name,
+            email,
+            password,
+            password2
+        })
+    }else{
+        //validation Passed
+        User.findOne({ email: email})
+        .then(user => {
+
+            if(user){
+                //User exits
+                errors.push({msg:'Email is already registered'});
+                res.render('register',{
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                })
+
+            }
+            else{
+const newUser = new User({
+    name,
+    email,
+    password
+});
+console.log(newUser)
+res.send('hello');
+            }
+        });
+    }
+
 });
 module.exports = router;
